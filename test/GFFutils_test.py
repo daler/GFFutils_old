c813cda23cb45d82b9feb51b633fb7e3ea2e8a01
Ryan <dalerr@niddk.nih.gov>
2010-06-04 19:14:07 -0400

    improved testing; new test for identical strings from db and from file

--- test/GFFutils_test.py
+++ test/GFFutils_test.py
@@ -43,30 +43,34 @@ GFF_parent_check_level_2 = {
 
 # Same thing for GTF test file . . .
 GTF_parent_check_level_1 = {
-                            'exon:FBtr0089256:1':['FBtr0089256'],
+                            'exon:FBtr0089256:7529-8116':['FBtr0089256'],
                             'FBtr0089256':['FBgn0031208'],
-                            'CDS:FBtr0089256:1':['FBtr0089256'],
-                            'start_codon:FBtr0089256:1':['FBtr0089256'],
-                            'exon:FBtr0089256:2':['FBtr0089256'],
-                            'CDS:FBtr0089256:2':['FBtr0089256'],
-                            'exon:FBtr0089256:3':['FBtr0089256'],
-                            'CDS:FBtr0089256:3':['FBtr0089256'],
-                            'stop_codon:FBtr0089256:1':['FBtr0089256'],
+                            'CDS:FBtr0089256:7680-8116':['FBtr0089256'],
+                            'start_codon:FBtr0089256:7680-7682':['FBtr0089256'],
+                            'exon:FBtr0089256:8229-8589':['FBtr0089256'],
+                            'CDS:FBtr0089256:8229-8589':['FBtr0089256'],
+                            'exon:FBtr0089256:8668-9491':['FBtr0089256'],
+                            'CDS:FBtr0089256:8668-9273':['FBtr0089256'],
+                            'stop_codon:FBtr0089256:9274-9276':['FBtr0089256'],
                            }
 GTF_parent_check_level_2 = {
-                            'exon:FBtr0089256:3':['FBgn0031208'],
-                            'CDS:FBtr0089256:3':['FBgn0031208'],
-                            'stop_codon:FBtr0089256:1':['FBgn0031208'],
-                            'start_codon:FBtr0089256:1':['FBgn0031208'],
-                            'exon:FBtr0089256:1':['FBgn0031208'],
-                            'CDS:FBtr0089256:1':['FBgn0031208'],
-                            'exon:FBtr0089256:2':['FBgn0031208'],
-                            'CDS:FBtr0089256:2':['FBgn0031208'],
+                            'exon:FBgn0031208:7529-8116':['FBgn0031208'],
+                            'FBgn0031208':['FBgn0031208'],
+                            'CDS:FBgn0031208:7680-8116':['FBgn0031208'],
+                            'start_codon:FBgn0031208:7680-7682':['FBgn0031208'],
+                            'exon:FBgn0031208:8229-8589':['FBgn0031208'],
+                            'CDS:FBgn0031208:8229-8589':['FBgn0031208'],
+                            'exon:FBgn0031208:8668-9491':['FBgn0031208'],
+                            'CDS:FBgn0031208:8668-9273':['FBgn0031208'],
+                            'stop_codon:FBgn0031208:9274-9276':['FBgn0031208'],
                            }
 
 class TestGFFDBClass(object):
     featureclass = 'GFF'
     def setup(self):
+        """
+        Creates a new GFFDB or GTFDB (depending on self.__class__.featureclass)
+        """
         if os.path.exists(testdbfn):
             os.remove(testdbfn)
 
@@ -91,7 +95,7 @@ class TestGFFDBClass(object):
         self.c = self.conn.cursor()
 
     def table_test(self):
-        """ensure the right tables exist"""
+        """Right tables exist?"""
         expected_tables = ['features','relations']
         expected_tables.sort()
         self.c.execute('select name from sqlite_master where type="table"')
@@ -101,13 +105,19 @@ class TestGFFDBClass(object):
         print observed_tables
         assert expected_tables == observed_tables
     
-    def count1(self,featuretype):
+    def _count1(self,featuretype):
+        """
+        Count using SQL
+        """
         self.c.execute('select count() from features where featuretype = ?',(featuretype,))
         results = self.c.fetchone()[0]
         print 'count1("%s") says: %s' % (featuretype,results)
         return results
 
-    def count2(self,featuretype):
+    def _count2(self,featuretype):
+        """
+        Count GFF lines.
+        """
         cnt = 0
         for line in open(self.fn):
             if line.startswith('#'):
@@ -122,12 +132,18 @@ class TestGFFDBClass(object):
         print 'count2("%s") says: %s' % (featuretype, cnt)
         return cnt
     
-    def count3(self,featuretype):
+    def _count3(self,featuretype):
+        """
+        Count with the count_features_of_type method.
+        """
         results = self.G.count_features_of_type(featuretype)
         print 'count3("%s") says: %s' % (featuretype, results)
         return results
     
-    def count4(self,featuretype):
+    def _count4(self,featuretype):
+        """
+        Count by iterating over all features of this type
+        """
         cnt = 0
         for i in self.G.features_of_type(featuretype):
             cnt += 1
@@ -135,7 +151,10 @@ class TestGFFDBClass(object):
         return cnt
     
     def featurecount_test(self):
-        """check the number of each featuretype, using 4 different ways of counting."""
+        """
+        Right number of each featuretype, using 3 different ways of
+        counting?
+        """
         featuretypes = ['gene',
                         'mRNA',
                         'CDS',
@@ -145,19 +164,25 @@ class TestGFFDBClass(object):
                         'pcr_product',
                         'protein','intron']
         for featuretype in featuretypes:
-            rawsql_cnt = self.count1(featuretype)
+            rawsql_cnt = self._count1(featuretype)
+            count_feature_of_type_cnt = self._count3(featuretype)
+            iterator_cnt = self._count4(featuretype)
 
-            # This is not an appropriate test for GTF files, since "gene" is
+            # count2 is not an appropriate test for GTF files, since "gene" is
             # not explicitly listed -- only implied by the boundaries of CDSs:
             #
-            # gffparsed_cnt = self.count2(featuretype)
-            #
-            count_feature_of_type_cnt = self.count3(featuretype)
-            iterator_cnt = self.count4(featuretype)
-            assert rawsql_cnt == count_feature_of_type_cnt == iterator_cnt
+            if self.featureclass == 'GFF':
+                gffparsed_cnt = self._count2(featuretype)
+                assert rawsql_cnt == count_feature_of_type_cnt == iterator_cnt == gffparsed_cnt
+            
+            if self.featureclass == 'GTF':
+                assert rawsql_cnt == count_feature_of_type_cnt == iterator_cnt
 
     def total_features_test(self):
         """did all features in the GFF file make it into the database?"""
+
+        # Doesn't make sense for GTF, which has gene features implied rather
+        # than explicitly stated.
         if self.featureclass == 'GTF':
             return
         cnt = 0
@@ -172,53 +197,49 @@ class TestGFFDBClass(object):
         assert cnt == total
 
     def parents_test(self):
-        "parents test"
+        "DB retrieval of parents matches expected?"
+        # Checks the hand-entered lookup dicts at the beginning of this module
+        # to make sure parents match up.
         if self.featureclass == 'GFF':
             parents1 = GFF_parent_check_level_1
             parents2 = GFF_parent_check_level_2
         if self.featureclass == 'GTF':
             parents1 = GTF_parent_check_level_1
             parents2 = GTF_parent_check_level_2
-
         for child, expected_parents in parents1.items():
             observed_parents = self.G.parents(child, level=1)
             for observed_parent in observed_parents:
+                print observed_parent
                 assert observed_parent.id in expected_parents
 
-    def _parents(self):
-        "parents test"
+    def identity_test(self):
+        """
+        Actual string in file identical to tostring() method output?
+        """
 
-        # The two transcripts have the same gene parent.
-        p1 = list(self.G.parents('FBtr0300690'))
-        p2 = list(self.G.parents('FBtr0300689'))
-        print p1
-        print p2
-        assert len(p1) == 1
-        assert len(p2) == 1
-        assert p1[0].id == p2[0].id
-
-        # Make sure that the inverse is true.
-        children_mRNAs = list(self.G.children(p1[0].id,featuretype='mRNA'))
-        print children_mRNAs
-        assert len(children_mRNAs) == 2 
-        
-        c = ['FBtr0300690', 'FBtr0300689']
-        for i in children_mRNAs:
-            assert i.id in c
-
-
-        # now spot-check for some second-level parents.
-        # Use a mix of exons, introns, UTRs, and CDSs.
-        grandchildren = ['unnamed_exon_1', 'FBgn0031208:3', 'CDS_FBgn0031208:2_737', 'intron_FBgn0031208:2_FBgn0031208:4']
-        
-        for grandchild in grandchildren:
-            grandparents = list(self.G.parents(grandchild,level=2))
-            assert len(grandparents) == 1
-            assert grandparents[0].id == 'FBgn0031208'
+        for i in open(self.fn):
+            if i.startswith('#'):
+                continue
+            first_line = i
+            break
+        if self.featureclass == 'GFF':
+            iterator = iter(GFFutils.GFFFile(self.fn))
+        if self.featureclass == 'GTF':
+            iterator = iter(GFFutils.GTFFile(self.fn))
+            
+        first_feature = iterator.next()
+        if self.featureclass == 'GTF':
+            first_feature.remove_attribute('ID')
+        print 'first line:',first_line
+        print 'first feat:',first_feature.tostring()
+        feature_string = first_feature.tostring()
+        if feature_string == first_line:
+            return
+        assert False
 
     def teardown(self):
         pass
-        os.remove(testdbfn)
+        #os.remove(testdbfn)
 
 class TestGTFDBClass(TestGFFDBClass):
     featureclass = 'GTF'
