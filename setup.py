60bb0d9e809e4c3092cbfd0df1e1e0b503bf57ec
daler <dalerr@niddk.nih.gov>
2010-07-07 17:26:47 -0400

    added GFF_cleaner.py to scripts in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup( 
         name='GFFutils',
-        version='0.1',
+        version='0.5',
         test_suite = 'test',
         py_modules = ['GFFutils'],
+        scripts=['scripts/flybase_GFF_cleaner.py'],
         author='Ryan Dale',
         url='none',
         author_email='dalerr@niddk.nih.gov'
